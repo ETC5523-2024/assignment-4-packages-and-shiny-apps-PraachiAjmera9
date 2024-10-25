610a1d575598ff07d009bcfbee0eb0dae4d1bdc9
PraachiAjmera9 <pajm0001@student.monash.edu>
2024-10-25 20:57:08 +1100

    Changes in docs folder

--- heritage/R/run_app.R
+++ heritage/R/run_app.R
@@ -1,11 +1,11 @@
-# R/launch_app.R
+# R/run_app.R
 
 #' Launch the Shiny App
 #'
 #' This function launches the Shiny app that lets users explore the World Heritage Sites dataset.
 #'
 #' @export
-launch_app <- function() {
+run_app <- function() {
   app_dir <- system.file("shiny", package = "heritage")
   if (app_dir == "") {
     stop("Could not find Shiny app directory. Try re-installing `heritage`.", call. = FALSE)
